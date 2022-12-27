ca0167356e10c3c8e9fa0c920121b655615837c8
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-27 06:27:41 -0300

    added useSignUp custom hook that makes an react...
    query mutation with the provided inputs and if the server response is
    succesfull store the new user token and information in AuthContext

---
+++ src/hooks/useSignUp.tsx
@@ -0,0 +1,58 @@
+import { useMutation } from '@tanstack/react-query';
+import axiosConfig from '../config/axiosConfig';
+import useAuth from './useAuth';
+import type { IUserInfo } from '../context/AuthContext';
+
+interface IFormInputs {
+  first_name: string;
+  last_name: string;
+  gender: GenderEnum;
+  birthday: string;
+  username: string;
+  password: string;
+  confirm_password: string;
+}
+
+enum GenderEnum {
+  female = 'female',
+  male = 'male',
+  other = 'other',
+}
+export type { IFormInputs, GenderEnum };
+
+interface IResponseData {
+  token: string;
+  userInfo: IUserInfo;
+}
+
+interface IAxiosErrors {
+  response: IErrorResponse;
+}
+
+interface IErrorResponse {
+  status: number;
+  data: { message: string; errors: IErrorMsg[] };
+}
+
+interface IErrorMsg {
+  msg: string;
+}
+
+async function postSignUp(userData: IFormInputs) {
+  const req = await axiosConfig.post(`/users/sign-up`, userData, {
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return req.data;
+}
+
+export default function UseSignup() {
+  const { setUserToken, setUserInfo } = useAuth();
+
+  return useMutation<IResponseData, IAxiosErrors, IFormInputs>({
+    mutationFn: (formInputs: IFormInputs) => postSignUp(formInputs),
+    onSuccess: (data) => {
+      setUserToken?.(data.token);
+      setUserInfo?.(data.userInfo);
+    },
+  });
+}
