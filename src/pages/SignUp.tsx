b1748d773193f39cb19c470ac14388151798b233
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-27 06:40:21 -0300

    added implementation of useSignUP custom hook...
    in the signUpForm component

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,9 @@
 import SignUpForm from '../components/SignUpForm';
 
 export default function SignUp() {
-  return <SignUpForm />;
+  return (
+    <main className="sign-up-page">
+      <SignUpForm />
+    </main>
+  );
 }
