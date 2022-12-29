e0210d362c55bb2988bf54e86f437b8b8ae82b19
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-29 00:41:56 -0300

    added useEffect that calls the useRefreshUser...
    custom hook in a interval of 10 minutes to refresh the current
    user access token

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import LoadingPage from './utils/LoadingPage';
 import PageLayout from './pages/PageLayout';
+import useRefreshUser from './hooks/useRefreshUser';
 
 const Home = lazy(() => import('./pages/Home'));
 const SignUp = lazy(() => import('./pages/SignUp'));
 const LogIn = lazy(() => import('./pages/LogIn'));
 
 function App() {
+  const refreshUser = useRefreshUser();
+
+  useEffect(() => {
+    refreshUser.mutate();
+
+    const refreshInterval = setInterval(() => {
+      refreshUser.mutate();
+    }, 1000 * 60 * 10);
+
+    return () => clearInterval(refreshInterval);
+  }, []);
+
   return (
     <div className="App">
       <Suspense fallback={<LoadingPage />}>
