b76e20f63fe09432e1fea26bec54a95865551bea
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-30 05:12:56 -0300

    added conditional rendering depending if there..
    is a user logged in

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,14 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { useState } from 'react';
 import useMountTransition from '../hooks/useMountTransition';
 import HamburgerMenu from './HamburgerMenu';
+import useAuth from '../hooks/useAuth';
+import ProfileDropDown from './ProfileDropDown';
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   const hasTransitionedIn = useMountTransition(isOpen, 300);
+
+  const { userToken } = useAuth();
 
   function openCloseMenu() {
     setIsOpen((prev) => !prev);
@@ -21,14 +25,20 @@ export default function NavBar() {
       </div>
       <div className="nav-bar__right-side">
         <ul className="nav-bar__list">
-          <li>
-            <Link to="/sign-up">Sign up</Link>
-          </li>
-          <li>
-            <Link to="/log-in">Log in</Link>
-          </li>
+          {!userToken ? (
+            <>
+              <li>
+                <Link to="/sign-up">Sign up</Link>
+              </li>
+              <li>
+                <Link to="/log-in">Log in</Link>
+              </li>
+            </>
+          ) : null}
+          <li>{userToken ? <ProfileDropDown /> : null}</li>
         </ul>
       </div>
+
       {!isOpen && !hasTransitionedIn ? (
         <button
           type="button"
