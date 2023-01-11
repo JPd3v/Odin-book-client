ec01a8652ffbe842864c6af4944267bd26489dcb
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-01-11 07:13:22 -0300

    implemented form submition with the custom hook...
    useNewReply

--- src/components/NewReplyForm.tsx
+++ src/components/NewReplyForm.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import useNewReply from '../hooks/useNewReply';
+import LoadingSpinner from './LoadingSpinner';
 
 interface IFormInputs {
-  reply_text: string;
+  replyText: string;
 }
 
 interface Iprops {
@@ -12,36 +15,71 @@ interface Iprops {
 export default function NewReplyForm({ commentId }: Iprops) {
   const {
     register,
+    reset,
     formState: { errors },
     handleSubmit,
   } = useForm<IFormInputs>({ mode: 'onChange' });
 
+  const newReplyMutation = useNewReply();
+
   const REPLY_UUID = crypto.randomUUID();
 
   function onSubmit(formInputs: IFormInputs) {
-    console.log(formInputs);
-    console.log(commentId);
+    const { replyText } = formInputs;
+    newReplyMutation.mutate({ reply_text: replyText, commentId });
   }
+
+  useEffect(() => {
+    if (newReplyMutation.isSuccess) {
+      reset();
+    }
+  }, [newReplyMutation.isSuccess, reset]);
 
   return (
     <form className="reply-form" onSubmit={handleSubmit(onSubmit)} noValidate>
       <textarea
         aria-labelledby={`reply-error${REPLY_UUID}`}
-        aria-invalid={errors.reply_text ? 'true' : 'false'}
+        aria-invalid={errors.replyText ? 'true' : 'false'}
         className="reply-form__text-input"
         placeholder="Write your reply"
-        {...register('reply_text', { required: "can't send an empty reply" })}
+        {...register('replyText', { required: "can't send an empty reply" })}
       />
+
       <div className="reply-form__controllers">
-        <button className="reply-form__submit" type="submit">
+        <button
+          className="reply-form__submit"
+          type="submit"
+          disabled={newReplyMutation.isLoading}
+        >
           Submit
         </button>
+
         <p
           className="reply-form__error-message"
           id={`reply-error${REPLY_UUID}`}
         >
-          {errors.reply_text?.message}
+          {errors.replyText?.message}
         </p>
+
+        {newReplyMutation.isError ? (
+          <div role="alert">
+            {newReplyMutation.error.response.data.errors?.map((error) => (
+              <p className="reply-form__error-message" key={error.msg}>
+                {error.msg}
+              </p>
+            ))}
+
+            <p className="reply-form__error-message">
+              {newReplyMutation.error.response.data.message}
+            </p>
+          </div>
+        ) : null}
+
+        {newReplyMutation.isLoading ? (
+          <div className="reply-form__loading-spinner">
+            <LoadingSpinner />
+          </div>
+        ) : null}
       </div>
     </form>
   );
