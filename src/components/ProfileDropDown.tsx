f695533a5eff2bfe364059299a3882a38ece636c
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-01-14 15:47:52 -0300

    removed closing drop down logic from useEffect...
    and remplaced with custom hook

--- src/components/ProfileDropDown.tsx
+++ src/components/ProfileDropDown.tsx
@@ -1,37 +1,26 @@
 import { useState, useRef, useEffect } from 'react';
 import useAuth from '../hooks/useAuth';
+import useClickOutsideRef from '../hooks/useClickOutsideRef';
 import useLogOut from '../hooks/useLogOut';
 
 export default function ProfileDropDown() {
   const [isOpen, setIsOpen] = useState(false);
   const dropDownRef = useRef<HTMLDivElement>(null);
-  const buttonRef = useRef<HTMLDivElement>(null);
   const { userInfo } = useAuth();
   const logOut = useLogOut();
 
+  const handleClickOutside = useClickOutsideRef(dropDownRef);
+
   useEffect(() => {
-    function handleClickOutside({ target }: MouseEvent) {
-      if (
-        dropDownRef.current &&
-        !dropDownRef.current.contains(target as Node) &&
-        !buttonRef.current?.contains(target as Node)
-      ) {
-        setIsOpen(false);
-      }
-    }
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, [dropDownRef, buttonRef]);
+    return setIsOpen(false);
+  }, [handleClickOutside]);
 
   return (
-    <>
+    <div className="profile-drop" ref={dropDownRef}>
       <button
         type="button"
         className="profile-drop-down__button"
         onClick={() => setIsOpen((prev) => !prev)}
-        ref={buttonRef as unknown as React.RefObject<HTMLButtonElement>}
       >
         <img
           src={`${userInfo?.profile_image}`}
@@ -40,25 +29,24 @@ export default function ProfileDropDown() {
         />
       </button>
       {isOpen ? (
-        <ul
-          className="profile-drop-down__list"
-          ref={dropDownRef as unknown as React.RefObject<HTMLUListElement>}
-        >
-          <li>
-            <p>{`${userInfo?.first_name} ${userInfo?.last_name}`}</p>
-          </li>
-          <li>Account settings</li>
-          <li>
-            <button
-              type="button"
-              className="profile-drop-down__log-out"
-              onClick={() => logOut.mutate()}
-            >
-              Log out
-            </button>
-          </li>
-        </ul>
+        <div>
+          <ul className="profile-drop-down__list">
+            <li>
+              <p>{`${userInfo?.first_name} ${userInfo?.last_name}`}</p>
+            </li>
+            <li>Account settings</li>
+            <li>
+              <button
+                type="button"
+                className="profile-drop-down__log-out"
+                onClick={() => logOut.mutate()}
+              >
+                Log out
+              </button>
+            </li>
+          </ul>
+        </div>
       ) : null}
-    </>
+    </div>
   );
 }
