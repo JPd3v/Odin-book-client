5b28b026acd57d650f19f199ae7157aa715b1aee
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-01-21 13:56:09 -0300

    removed trash

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -11,7 +11,6 @@ export default function UserProfile() {
   const user = useUserProfile();
   const friendList = user.data?.friend_list ?? [];
 
-  console.log(user.error?.response?.status);
   function checkFriend() {
     for (let index = 0; index < friendList.length; index += 1) {
       const element = friendList[index];
@@ -62,36 +61,3 @@ export default function UserProfile() {
     </>
   );
 }
-// return user.isLoading || user.isError ? (
-//   user.isError ? (
-//     <PageNotFound404 />
-//   ) : (
-//     <p>loading</p>
-//   )
-// ) : (
-//   <>
-//     <div className="user-profile">
-//       <img
-//         src={`${user.data?.profile_image.img}`}
-//         alt=""
-//         className="user-profile__img"
-//       />
-//       <div>
-//         <h2 className="user-profile__name">{`${user.data?.first_name} ${user.data?.last_name}`}</h2>
-//         {isFriend ? <p>are Friends</p> : null}
-//         {haveFriendRequest ? <p>pending request</p> : null}
-//         {!haveFriendRequest &&
-//         !isFriend &&
-//         user.data?._id !== userInfo?._id ? (
-//           <p>add friend</p>
-//         ) : null}
-//       </div>
-//     </div>
-//     <div className="user-friends">
-//       <p>Friends</p>
-//       {user.data?.friend_list.slice(0, 20).map((friend) => (
-//         <FriendCard key={friend._id} friend={friend} />
-//       ))}
-//     </div>
-//   </>
-// );
