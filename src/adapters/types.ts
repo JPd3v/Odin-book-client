70c571861aa1c36c0300ac9fe962241c1d91eac3
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-02-08 23:40:34 -0300

    adds userAdapter to adapt server response
    to user used in the App

---
+++ src/adapters/types.ts
@@ -0,0 +1,15 @@
+interface ProfileImage {
+  img: string;
+}
+
+interface ServerUserInfo {
+  profile_image: ProfileImage;
+  _id: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  gender: string;
+  birthday: string;
+}
+
+export type { ServerUserInfo };
---
+++ src/adapters/userAdapter.ts
@@ -0,0 +1,14 @@
+// import { IUserInfo } from "context";
+import { ServerUserInfo } from './types';
+
+export default function createUserAdapter(user: ServerUserInfo) {
+  return {
+    _id: user._id,
+    first_name: user.first_name,
+    last_name: user.last_name,
+    profile_image: user.profile_image.img,
+    gender: user.gender,
+    birthday: user.birthday,
+    email: user.username,
+  };
+}
