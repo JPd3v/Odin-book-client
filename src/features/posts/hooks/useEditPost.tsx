680f1f360cf9fb02ab1e270524dd7e664423cb9f
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-07-05 20:42:34 -0300

    changes how post edit hook works

--- src/features/posts/hooks/useEditPost.tsx
+++ src/features/posts/hooks/useEditPost.tsx
@@ -2,9 +2,11 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosConfig } from 'config/index';
 import { useAuth } from 'hooks/index';
 import type { ITextEdition, ITextEditionErrors } from 'components/common/index';
+import { InfiniteData } from 'features/posts/types';
+import { IPost } from 'types';
 
 async function editPost({ text, id }: ITextEdition, userToken: string) {
-  const req = await axiosConfig.put(
+  const req = await axiosConfig.put<IPost>(
     `/posts/${id}/`,
     {
       content: {
@@ -19,12 +21,22 @@ async function editPost({ text, id }: ITextEdition, userToken: string) {
 export default function useEditPost(queryKey: string | Array<string | number>) {
   const { userToken } = useAuth();
   const queryClient = useQueryClient();
-  return useMutation<unknown, ITextEditionErrors, ITextEdition, unknown>(
+  return useMutation<IPost, ITextEditionErrors, ITextEdition, unknown>(
     [queryKey],
     {
       mutationFn: (data: ITextEdition) => editPost(data, userToken ?? ''),
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: [queryKey] });
+      onSuccess: (editedPost) => {
+        queryClient.setQueryData<InfiniteData>([queryKey], (prev) => {
+          return {
+            ...prev,
+            pages: prev?.pages?.map((page) => ({
+              ...page,
+              posts: page.posts.map((post) =>
+                post._id === editedPost._id ? editedPost : post
+              ),
+            })),
+          };
+        });
       },
     }
   );
