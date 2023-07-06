291281e3627b435016fc3c8983b7cb5c5a1b4a6e
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-07-05 21:23:35 -0300

    added optimistic updates on post deletion

--- src/features/posts/hooks/useDeletePost.tsx
+++ src/features/posts/hooks/useDeletePost.tsx
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosConfig } from 'config/index';
+import { InfiniteData } from 'features/posts/types';
 import { useAuth } from 'hooks/index';
 
 async function deletePost(postId: string, userToken: string) {
   const req = await axiosConfig.delete(`/posts/${postId}`, {
     headers: { authorization: `Bearer ${userToken}` },
   });
-
   return req.data;
 }
 
@@ -17,8 +17,27 @@ export default function useDeletePost(
   const queryClient = useQueryClient();
   return useMutation([queryKey], {
     mutationFn: (postId: string) => deletePost(postId, userToken ?? ''),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [queryKey] });
+    onMutate: async (postId) => {
+      await queryClient.cancelQueries([queryKey]);
+
+      const previousPosts = queryClient.getQueryData<InfiniteData>([queryKey]);
+      queryClient.setQueryData<InfiniteData>([queryKey], (prev) => {
+        return {
+          ...prev,
+          pages: prev?.pages?.map((page) => ({
+            ...page,
+            posts: page.posts.filter((post) => post._id !== postId),
+          })),
+        };
+      });
+
+      return { previousPosts };
+    },
+    onError: (_error, _var, context) => {
+      queryClient.setQueryData<InfiniteData>(
+        [queryKey],
+        context?.previousPosts
+      );
     },
   });
 }
