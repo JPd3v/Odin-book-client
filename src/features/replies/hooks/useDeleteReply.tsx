156539e4f579d5a87fe194e7eebc7b14934c9d4c
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-07-12 23:22:01 -0300

    adds util function and updates useDeleteReply hook
    -adds util function to help delete reply from cache

--- src/features/replies/hooks/useDeleteReply.tsx
+++ src/features/replies/hooks/useDeleteReply.tsx
@@ -1,6 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosConfig } from 'config/index';
+import replyKeys from 'features/replies/utils/replyQuerykeyFactory';
+import { deleteReplyInCache } from 'features/replies/utils/updateInfiniteRepliesCache';
 import { useAuth } from 'hooks/index';
+import { IReply, InfiniteReplies } from 'types';
 
 async function deleteReply(replyId: string, userToken: string) {
   const req = await axiosConfig.delete(`/replies/${replyId}`, {
@@ -10,15 +13,34 @@ async function deleteReply(replyId: string, userToken: string) {
   return req.data;
 }
 
-export default function useDeleteReply(
-  queryKey: string | Array<string | number>
-) {
+export default function useDeleteReply() {
   const { userToken } = useAuth();
   const queryClient = useQueryClient();
-  return useMutation([queryKey], {
-    mutationFn: (replyId: string) => deleteReply(replyId, userToken ?? ''),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [queryKey] });
+  return useMutation({
+    mutationFn: (reply: IReply) => deleteReply(reply._id, userToken as string),
+    onMutate: async (deletedReply) => {
+      const commentId = deletedReply.comment_id;
+      const replyId = deletedReply._id;
+
+      await queryClient.cancelQueries(replyKeys.reply(commentId));
+
+      const previousReplies = queryClient.getQueryData<InfiniteReplies>(
+        replyKeys.reply(commentId)
+      );
+
+      queryClient.setQueryData<InfiniteReplies>(
+        replyKeys.reply(commentId),
+        (prev) => deleteReplyInCache(prev, replyId)
+      );
+
+      return { previousReplies };
+    },
+    onError: (_error, reply, context) => {
+      const commentId = reply.comment_id;
+      queryClient.setQueryData<InfiniteReplies>(
+        replyKeys.reply(commentId),
+        context?.previousReplies
+      );
     },
   });
 }
--- src/features/replies/utils/updateInfiniteRepliesCache.ts
+++ src/features/replies/utils/updateInfiniteRepliesCache.ts
@@ -13,4 +13,18 @@ function newReplyOnCache(prev: InfiniteReplies | undefined, newReply: IReply) {
   };
 }
 
-export { newReplyOnCache };
+function deleteReplyInCache(
+  prev: InfiniteReplies | undefined,
+  deletedReplyId: string
+) {
+  if (!prev) return undefined;
+  return {
+    ...prev,
+    pages: prev?.pages.map((page) => ({
+      ...page,
+      replies: page.replies.filter((reply) => reply._id !== deletedReplyId),
+    })),
+  };
+}
+
+export { newReplyOnCache, deleteReplyInCache };
